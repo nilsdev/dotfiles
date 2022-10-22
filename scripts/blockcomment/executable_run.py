e8f99052634e1b154c0991edb6aa8a456505f8d5
nilsdev <nilsdev+1@protonmail.com>
2022-10-22 18:49:53 +0200

    Update scripts/blockcomment/run.py

--- scripts/blockcomment/executable_run.py
+++ scripts/blockcomment/executable_run.py
@@ -2,9 +2,12 @@
 
 from sys import argv
 
+┌─────┐
+│ hey │
+└─────┘
+
 del argv[0]
 input = ' '.join(argv)
-print(input)
 
 top_border = "┌" + "─" * (len(input) + 2) + "┐"
 bottom_border = "└" + "─" * (len(input) + 2) + "┘"
